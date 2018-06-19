410999b4e8def331b31fafb8921103b287fe15b7
Sven Werner <vegascowboy@web.de>
2018-06-19 17:12:25 +0200

    Created routes and views for booking, and added basic navigation through those pages

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,27 @@
+class BookingsController < ApplicationController
+
+
+
+  def new
+    @booking = Booking.new
+    @user = current_user
+  end
+
+  def create
+    @booking = Booking.new
+    @booking.user = current_user
+    @booking.save
+
+    redirect_to booking_path(@booking)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+end
---
+++ test/controllers/bookings_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class BookingsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get bookings_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get bookings_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get bookings_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get bookings_update_url
+    assert_response :success
+  end
+
+  test "should get confirm" do
+    get bookings_confirm_url
+    assert_response :success
+  end
+
+  test "should get congratulation" do
+    get bookings_congratulation_url
+    assert_response :success
+  end
+
+end
