425952feb5d0527290e470501e910ec42570b4ec
Sven Werner <vegascowboy@web.de>
2018-06-25 19:05:42 +0200

    Navbar side

--- app/javascript/packs/navbar_side.js
+++ app/javascript/packs/navbar_side.js
@@ -1,33 +1,29 @@
-
-
 export default function scrollTab() {
-  const navBarSteps = document.querySelectorAll(".nav-side-step")
-  const indexnum = document.querySelectorAll("h1");
-  const triggerOffset = 150;
 
-  window.addEventListener("scroll", function(){
-    for(let i = 0; i < indexnum.length; i += 1) {
-      if (checkInView(indexnum[i], 175)) {
-        navBarSteps[i].classList.add("active");
-        console.log("active")
-      } else {
+  const bookingNewPageID = document.querySelector(".nav-side-step")
+  if (bookingNewPageID != null){
+    const navBarSteps = document.querySelectorAll(".nav-side-step")
+    const indexnum = document.querySelectorAll("h1");
+    const offSet = 200
+
+    window.addEventListener("scroll", function(){
+      for(let i = 0; i < indexnum.length; i += 1) {
+        if (checkInView(indexnum[i], offSet)) {
+          navBarSteps[i].classList.add("active");
+        } else {
          navBarSteps[i].classList.remove("active");
-        console.log("not active")
-      }
-    }
-  }, false);
+       }
+     }
+   }, false);
 
-
-  function checkInView(element, triggerOffset) {
+    function checkInView(element, offSet) {
       const windowHeight = window.innerHeight;
-       const elementTopToPageTop = element.getBoundingClientRect().top;
-        // const elementTopToPageBottom = elementTopToPageTop - windowHeight;
-         if (elementTopToPageTop > 0 && elementTopToPageTop < (windowHeight)
-        // && elementTopToPageBottom > triggerOffset
-              ) {
-          return true
-        }
-        return false
+      const elementTopToPageTop = element.getBoundingClientRect().top;
+      if (elementTopToPageTop > - offSet && elementTopToPageTop < (windowHeight - offSet)) {
+        return true
+      }
+      return false
+    }
   }
 }
 
