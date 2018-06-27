b01a1864f5b03f14772efec8f75a8f284793dcbc
Willy Koeplin <willykoeplin@gmail.com>
2018-06-27 13:01:23 +0200

    readded validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,13 +1,13 @@
 class Booking < ApplicationRecord
-  # validates :origin, presence: true
-  # validates :budget_pp, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 150, less_than_or_equal_to: 1000 }
-  # validates :climate, presence: true, inclusion: { in: %w(warm cold surprise),
-  #   message: "%{value} is not a valid climate" }
-  # validates :accommodation_type, presence: true, inclusion: { in: ["Hostel", "Hotel", "Bed and Breakfast", "Luxury Hotel"]}
-  # validates :type_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
-  # validates :number_traveller, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 6 }
-  # validates :start_time, presence: true
-  # validate :start_time_cannot_be_in_the_past
+  validates :origin, presence: true
+  validates :budget_pp, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 150, less_than_or_equal_to: 1000 }
+  validates :climate, presence: true, inclusion: { in: %w(warm cold surprise),
+    message: "%{value} is not a valid climate" }
+  validates :accommodation_type, presence: true, inclusion: { in: ["Hostel", "Hotel", "Bed and Breakfast", "Luxury Hotel"]}
+  validates :type_id, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 4 }
+  validates :number_traveller, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 6 }
+  validates :start_time, presence: true
+  validate :start_time_cannot_be_in_the_past
 
   has_many :travellers
   belongs_to :package, required: false
