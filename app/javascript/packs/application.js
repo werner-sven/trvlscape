d1b9e49691db5cab167959360ae14fc0ebf261e8
Sven Werner <vegascowboy@web.de>
2018-06-27 13:43:45 +0200

    navbar adjust and small fixes - i don't care about commits

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,12 @@ import "bootstrap";
 import { slider } from '../modules/slider';
 import { scrollWin } from '../modules/push';
 import scrollTab from '../packs/navbar_side';
+import navbar from '../packs/navbar';
 import { loadDynamicBannerText } from '../modules/animated_typing';
 loadDynamicBannerText();
 slider();
 scrollWin();
 scrollTab();
+navbar();
 
 
