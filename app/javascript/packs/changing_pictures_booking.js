7300671196348376021ea1174c376a3b68070746
Willy Koeplin <willykoeplin@gmail.com>
2018-06-28 11:34:01 +0200

    logic finished

--- app/javascript/packs/changing_pictures_booking.js
+++ app/javascript/packs/changing_pictures_booking.js
@@ -1,10 +1,20 @@
 const numberTravellers = document.querySelector("#form-traveller-number")
 const buttonUp = document.querySelector("#form-button-increase");
 const buttonDown = document.querySelector("#form-button-decrease");
+const sunButton = document.querySelector("#warm-weather");
+const cloudButton = document.querySelector("#cold-weather");
+const questionMarkButton = document.querySelector("#surprise-weather");
+
+if (numberTravellers != null) {
+
+getNumberTravellers();
+setQuestionMark();
 
 buttonUp.addEventListener("click", getNumberTravellers);
 buttonDown.addEventListener("click", getNumberTravellers);
-
+sunButton.addEventListener("click", setSun);
+cloudButton.addEventListener("click", setCloud);
+questionMarkButton.addEventListener("click", setQuestionMark);
 
 function getNumberTravellers() {
 if (numberTravellers.value === "1") {
@@ -16,12 +26,16 @@ if (numberTravellers.value === "1") {
 }
 }
 
-function getTravellersAndClimate() {
-if (numberTravellers.value === "1") {
-
-} else if (numberTravellers.value === "2") {
-  console.log("2");
-} else if (numberTravellers.value === "3") {
-  console.log("3");
+function setSun() {
+  console.log("sun")
 }
+
+function setCloud() {
+  console.log("cloud")
+}
+
+function setQuestionMark() {
+  console.log("?")
+}
+
 }
