6a7aac419a05cafbbda600782bb599a0650c7f91
Willy Koeplin <willykoeplin@gmail.com>
2018-06-29 18:00:42 +0200

    changed SMSses

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -9,19 +9,27 @@ class TwilioService
     @to = to
   end
 
-  def confirmation(user_booking)
-    @client.messages.create(
-      from: @from,
-      to: @to,
-      body: "Hello #{user_booking.first_name}. Your surprise trip is fully booked. Just kick back and relax."
-    )
-  end
-
   def weather(body)
     @client.messages.create(
       from: @from,
       to: @to,
       body: body
+    )
+  end
+
+  def confirmation(user_booking)
+    @client.messages.create(
+      from: @from,
+      to: @to,
+      body: "Hello #{user_booking.first_name}. It is time for your flight. Your flight will be to ... drumroll..."
+    )
+  end
+
+  def reveal(booking)
+    @client.messages.create(
+      from: @from,
+      to: @to,
+      body: "#{booking.package.city} in #{booking.package.country} 🎉"
     )
   end
 end
